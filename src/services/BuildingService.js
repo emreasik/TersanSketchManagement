27f7125aa98b47a2ad489dd451947afcd35803a7
emreasik <asik.emre@outlook.com>
2023-02-07 22:56:07 +0300

    update func added for building service

--- src/services/BuildingService.js
+++ src/services/BuildingService.js
@@ -2,22 +2,43 @@ export default class BuildingService {
     constructor(fetcher) {
         this.fetcher = fetcher;
     }
-
-    async getBuildings() {
-        const data = await this.fetcher.get("/Building/GetAll");
-        console.log(data.data.data);
-        return data.data.data;
+    getBuildings() {
+        return this.fetcher.post("/Building/GetSummary", {
+            pageSize: 100,
+            pageNumber: 1,
+            sketchId: 1
+        }).then(function (response) {
+            return response.data.data;
+        })
+            .catch(function (error) {
+                console.log(error);
+            }
+            );
     }
-
-    async addBuilding(buildingData) {
-        return await this.fetcher.post("/Building", {
+    addBuilding(buildingData) {
+        return this.fetcher.post("/Building", {
             name: buildingData.name,
             x: buildingData.x,
             y: buildingData.y,
             sketchId: buildingData.sketchId,
         }).then(function (response) {
-            alert("Building added successfully!")
+            return response.data;
+        })
+            .catch(function (error) {
+                console.log(error);
+            }
+            );
+    }
+    updateBuilding(updateBuildingData) {
+        return this.fetcher.put("/Building", {
+            id: updateBuildingData.id,
+            name: updateBuildingData.name,
+            x: updateBuildingData.x,
+            y: updateBuildingData.y,
+        }).then(function (response) {
+            alert("Building updated successfully!")
             console.log(response);
+            return response.data;
         })
             .catch(function (error) {
                 console.log(error);

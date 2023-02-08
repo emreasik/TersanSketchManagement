a113ba691fdf1ef3e1658b86a0b54584c17d434a
emreasik <asik.emre@outlook.com>
2023-02-08 11:35:17 +0300

    new toast objects added

--- src/helpers/toastConstants.js
+++ src/helpers/toastConstants.js
@@ -1,37 +1,101 @@
-const SUCCCESS = (messageType) => {
+const SUCCCESS_ADD = (messageType) => {
     return import.meta.env.VITE_APP_LANG === 'TR' ?
-    {
-        title: 'Başarılı',
-        message: `Yeni ${messageType} başarıyla eklendi`,
-        type: 'success'
-    }
-    :
-    {
-        title: 'Success',
-        message: `New ${messageType} added successfully`,
-        type: 'success'
-    }
+        {
+            title: 'Başarılı',
+            message: `Yeni ${messageType} başarıyla eklendi`,
+            type: 'success'
+        }
+        :
+        {
+            title: 'Success',
+            message: `New ${messageType} added successfully`,
+            type: 'success'
+        }
 }
 
-const ERROR = (messageType) => {
+const SUCCCESS_UPDATE = (messageType) => {
     return import.meta.env.VITE_APP_LANG === 'TR' ?
-    {
-        title: 'Hata',
-        message: `Yeni ${messageType} eklenirken bir hata oluştu`,
-        type: 'error'
-    }
-    :
-    {
-        title: 'Error',
-        message: `An error occurred while adding ${messageType}`,
-        type: 'error'
-    }
+        {
+            title: 'Başarılı',
+            message: `${messageType} başarıyla düzenlendi`,
+            type: 'success'
+        }
+        :
+        {
+            title: 'Success',
+            message: `${messageType} updated successfully`,
+            type: 'success'
+        }
 }
 
+const SUCCCESS_DELETE = (messageType) => {
+    return import.meta.env.VITE_APP_LANG === 'TR' ?
+        {
+            title: 'Başarılı',
+            message: `${messageType} başarıyla silindi`,
+            type: 'success'
+        }
+        :
+        {
+            title: 'Success',
+            message: `${messageType} deleted successfully`,
+            type: 'success'
+        }
+}
+
+
+const ERROR_ADD = (messageType) => {
+    return import.meta.env.VITE_APP_LANG === 'TR' ?
+        {
+            title: 'Hata',
+            message: `Yeni ${messageType} eklenirken bir hata oluştu`,
+            type: 'error'
+        }
+        :
+        {
+            title: 'Error',
+            message: `An error occurred while adding ${messageType}`,
+            type: 'error'
+        }
+}
+
+const ERROR_UPDATE = (messageType) => {
+    return import.meta.env.VITE_APP_LANG === 'TR' ?
+        {
+            title: 'Hata',
+            message: `${messageType} düzenlenirken bir hata oluştu`,
+            type: 'error'
+        }
+        :
+        {
+            title: 'Error',
+            message: `An error occurred while updating ${messageType}`,
+            type: 'error'
+        }
+}
+
+const ERROR_DELETE = (messageType) => {
+    return import.meta.env.VITE_APP_LANG === 'TR' ?
+        {
+            title: 'Hata',
+            message: `${messageType} silinirken bir hata oluştu`,
+            type: 'error'
+        }
+        :
+        {
+            title: 'Error',
+            message: `An error occurred while adding ${messageType}`,
+            type: 'error'
+        }
+}
 
 const toastMessages = {
-    SUCCCESS,
-    ERROR
+    SUCCCESS_ADD,
+    SUCCCESS_UPDATE,
+    SUCCCESS_DELETE,
+    ERROR_ADD,
+    ERROR_UPDATE,
+    ERROR_DELETE
 }
 
 export default toastMessages;
